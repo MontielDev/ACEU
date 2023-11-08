d96f2aa9bf306ad3addea318313e84184a6cfab5
CamiloMontiel99 <camilo__developer@hotmail.com>
2023-11-08 15:00:12 -0500

    Updated

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,11 @@ setInterval(function() {
         counter = 1;
     }
 }, 7000)
+
+
+async function getData() {
+    const res = await fetch ("https://console.cloudinary.com/pm/c-6383c532d2ab63fb4fc295f022e126/media-explorer/ACEU/Galeria");
+    console.log(res);
+}
+
+getData();
